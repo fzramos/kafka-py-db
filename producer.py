43bd11514dc6640957b3bbc8a57c3739121876e1
Freddie Ramos <fred.ramos@capgemini.com>
2021-04-12 11:37:08 -0500

    Adding shutdown hook to close producer when program ends or is interrupted

--- producer.py
+++ producer.py
@@ -1,6 +1,7 @@
 from time import sleep
 from json import dumps
 from kafka import KafkaProducer
+import atexit
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                         value_serializer=lambda x:
@@ -9,8 +10,15 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
 # value_serializer sets how value should be serialized before sending to broker
 # lambda x:dumps(x).encode('utf-8') converts the data to a JSON file and encodes it to utf-8
 
+# when program ends or is shutdown, this code will close the producer
+@atexit.register
+def cleanup():
+    print('Shutting down producer')
+    producer.close()
+
 for e in range(1000):
     data = {'number': e}
-    producer.send('numtest', value=data)
+    producer.send('numtest', key=b'test', value=data)
     # key although data is a key value pair, data itself is the value and it has a null topic key
     sleep(3)
+    
